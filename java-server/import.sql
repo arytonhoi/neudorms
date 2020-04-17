1380407936a7d9bac68f606fbed79628e01e0996
Stanley Liu <liustan24@gmail.com>
2020-04-17 17:18:48 -0400

    trying import.sql

---
+++ java-server/import.sql
@@ -0,0 +1,17 @@
+--INSERT INTO buildings (address, main_image_url, name, thumbnail_image_url) VALUES
+--	("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Stetson West", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/STW-exterior-Speare-Quad-03-e1511889294218.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Stetson East", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/STW-exterior-Speare-Quad-03-e1511889294218.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Melvin Hall", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Speare Hall", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/spearehall.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Smith Hall", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Light Hall", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Kerr Hall", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "153 Hemenway Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "Kennedy Hall", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "International Village", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/INV-exterior-01.jpg"),
+--    ("21 Forsyth Street", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/WHITE-exterior-02-e1511889474120.jpg", "East Village", "https://www.northeastern.edu/housing/wp-content/uploads/2017/05/exterior-04.jpg");
+--
+INSERT INTO buildings (id, name) VALUES
+    (1, "name1"),
+    (2, "name2"),
+    (3, "name3");
