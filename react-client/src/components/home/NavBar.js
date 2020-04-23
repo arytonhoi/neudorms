d3dc232982e8cc9be8d79295bced44ce8935a275
Stanley Liu <liustan24@gmail.com>
2020-04-23 12:17:29 -0400

    fixed navbar responsive

--- react-client/src/components/home/NavBar.js
+++ react-client/src/components/home/NavBar.js
@@ -1,64 +1,76 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/js/dist/collapse";
+import $ from "jquery";
+import "./navbar.css";
 
 export default function NavBar(props) {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light p-3">
-        <Link className="navbar-brand mr-5 ml-3" to="/">
-          neudorms
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse mr-3" id="navbarText">
-          <ul className="navbar-nav mr-auto" />
-          {props.loggedIn && (
-            <div className="row">
-              {/* <a className="nav-link profile" href="#">
-                Housing Group
-              </a> */}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light p-3">
+      <Link className="navbar-brand mr-5 ml-3" to="/">
+        neudorms
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={() => $("#navbarText").toggleClass("justify-content-end")}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse ml-3 mr-3 justify-content-end" id="navbarText">
+        {props.loggedIn && (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
               <Link className="nav-link profile" to="/search">
                 Search
               </Link>
-              {props.role === "user" && (
+            </li>
+            {props.role === "user" && (
+              <li className="nav-item">
                 <Link className="nav-link profile" to="/bookmarks">
-                Bookmarks
-              </Link>
-              )}
-              <Link
-                className="nav-link profile"
-                to={`/profile`}
-              >
+                  Bookmarks
+                </Link>
+              </li>
+            )}
+
+            <li className="nav-item">
+              <Link className="nav-link profile" to={`/profile`}>
                 Profile
               </Link>
-              <Link
-                className="nav-link profile"
-                to="#"
-                onClick={props.logout}
-              >
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link profile" to="#" onClick={props.logout}>
                 Log Out
               </Link>
-            </div>
-          )}
-          {!props.loggedIn && (
-            <div className="row">
+            </li>
+          </ul>
+        )}
+        {!props.loggedIn && (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link profile" to="/search">
+                Search
+              </Link>
+            </li>
+            <li className="nav-item">
               <Link className="nav-link profile" to="/login">
                 Log In
               </Link>
+            </li>
+
+            <li className="nav-item">
               <Link className="nav-link profile" to="/register">
                 Sign Up
               </Link>
-            </div>
-          )}
-        </div>
-      </nav>
-    );
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
 }
